11a6e72f3adfa2b08ed4da3397055af2c1d322ce
Abel <abel.alvarez@galileo.edu>
2024-04-30 02:09:07 -0600

    feat: :bug: Arreglando errores de deploy

--- src/components/LoginPage/[...index].jsx
+++ src/components/LoginPage/[...index].jsx
@@ -32,18 +32,18 @@ export default function LoginPage() {
   return (
     <>
     <div className='grid md:grid-cols-2 gap-1'>
-      <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div class="max-w-md w-full space-y-8">
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
             <div>
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Iniciar sesión</h2>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Iniciar sesión</h2>
             </div>
-            <form class="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <div>
-                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <label for="email" className="block text-sm font-medium text-gray-700">Email</label>
                 <input id="email" name="email" type="email" autocomplete="email" required className="custom-input" placeholder="Email" />
             </div>
             <div>
-                <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
+                <label for="password" className="block text-sm font-medium text-gray-700">Contraseña</label>
                 <input id="password" name="password" type="password" autocomplete="current-password" required className='custom-input' placeholder="Contraseña" />
             </div>
             <div>
