45cd99a35e173311821457bbc0d430f0802bb9c9
Abel <abel.alvarez@galileo.edu>
2024-04-30 01:07:29 -0600

    feat: :art: cambio formato

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import 'tailwindcss/tailwind.css';
+import LoginPage from '../components/LoginPage/[...index]';
+
+export default function Home() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+
+
+}
