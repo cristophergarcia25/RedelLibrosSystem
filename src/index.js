76f778b42ac9043d6bc5117295fb6224a0e404b3
Abel <abel.alvarez@galileo.edu>
2024-04-30 00:33:22 -0600

    feat: :art: Pruebas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
-import LoginPage from '../components/LoginPage/[...index]';
+import LoginPage from './components/LoginPage/[...index]';
 // import '../styles/global.css'; // Importa las clases de globals.css
 
 export default function Home() {
@@ -9,9 +9,6 @@ export default function Home() {
     <>
       <LoginPage />
     </>
-    // <h1 className={styles.title}>
-    //   Read <Link href="/posts/first-post">this page!</Link>
-    // </h1>
   );
 
 
