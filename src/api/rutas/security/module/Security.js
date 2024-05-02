1e772b0ff1e8a1bee1a662186551bc5001a9721e
David Barrera <20002878@galileo.edu>
2024-05-02 17:58:33 -0600

    Comentarios

--- src/api/rutas/security/module/Security.js
+++ src/api/rutas/security/module/Security.js
@@ -3,6 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export class Security {
+  /**
+   * The function `login` is an asynchronous function that attempts to find a user in the database
+   * based on the provided email and password parameters. If the user is not found, it returns an error
+   * message.
+   * @param params - {
+   * @returns If the login is successful, the function will return the response from the database query
+   * which contains the user information. If the login is unsuccessful (user not found or incorrect
+   * credentials), it will return an error object with the message "Usuario no encontrado" and details
+   * "El usuario o contraseña estan incorrectos". If an error occurs during the execution of the
+   * function, it will return the error object.
+   */
   async login(params) {
     try {
       const loginResponse = await prisma.usuario.findFirst({
--- src/api/rutas/usuarios/module/Usuario.js
+++ src/api/rutas/usuarios/module/Usuario.js
@@ -3,6 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export class Usuario {
+  /**
+   * The function "crearUsuario" creates a new user with admin role using the provided parameters.
+   * @param params - {
+   * @returns If the user creation is successful, the function will return the response object
+   * containing the newly created user's information. If there is an error during the creation process,
+   * the function will return the error message.
+   */
   async crearUsuario(params) {
     try {
       const crearResponse = await prisma.usuario.create({
@@ -23,6 +30,16 @@ export class Usuario {
     }
   }
 
+  /**
+   * The function "obtenerUsuario" asynchronously retrieves a user from a database using the provided
+   * ID and handles errors appropriately.
+   * @param id - The `id` parameter in the `obtenerUsuario` function is used to specify the unique
+   * identifier of the user that you want to retrieve from the database. This function uses the `id` to
+   * query the database and find the user with the matching identifier.
+   * @returns If the `obtenerUsuario` function successfully retrieves a user with the specified `id`,
+   * it will return the user object. If there is an error during the process, it will return the error
+   * message.
+   */
   async obtenerUsuario(id) {
     try {
       const obtenerUsuarioResponse = await prisma.usuario.findFirst({
