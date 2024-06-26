406712d9bcb45797add26caec48c5dc6fb7dd8ef
David Barrera <20002878@galileo.edu>
2024-06-25 19:16:43 -0600

    Endpoints proveedor

--- backend/rutas/proveedor/modules/Proveedor.ts
+++ backend/rutas/proveedor/modules/Proveedor.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { ICrearProveedorParams } from "./types";
+import { IActualizarProveedorParams, ICrearProveedorParams } from "./types";
 
 const prisma = new PrismaClient();
 
@@ -22,6 +22,76 @@ export class Proveedor {
         };
 
       return { success: true, data: crearProveedorResponse };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+
+  async actualizarProveedor(params: IActualizarProveedorParams) {
+    try {
+      const actualizarProveedorResponse = await prisma.proveedor.update({
+        where: {
+          id: params.id,
+        },
+        data: {
+          ...(params.direccion && { direccion: params.direccion }),
+          ...(params.correo && { correo: params.correo }),
+          ...(params.nombre && { nombre: params.nombre }),
+          ...(params.telefono && { telefono: params.telefono }),
+        },
+      });
+      if (!actualizarProveedorResponse)
+        return {
+          success: false,
+          error: "Proveedor no actualizado",
+          detalle: "Hubo un error al actualizar el proveedor",
+        };
+
+      return { success: true, data: actualizarProveedorResponse };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+
+  async listarProveedores() {
+    try {
+      const listarProveedoresResponse = await prisma.proveedor.findMany({
+        select: {
+          id: true,
+          telefono: true,
+          correo: true,
+          direccion: true,
+          nombre: true,
+        },
+      });
+      if (!listarProveedoresResponse)
+        return {
+          success: false,
+          error: "No existen proveedor",
+          detalle: "No existen proveedores para listar",
+        };
+
+      return { success: true, data: listarProveedoresResponse };
+    } catch (error) {
+      return { success: false, error: error };
+    }
+  }
+
+  async borrarProveedor(id: string) {
+    try {
+      const borrarProveedorResponse = await prisma.proveedor.delete({
+        where: {
+          id: id,
+        },
+      });
+      if (!borrarProveedorResponse)
+        return {
+          success: false,
+          error: "Proveedor no eliminado",
+          detalle: "Ocurrio un error al tratar de eliminar el proveedor",
+        };
+
+      return { success: true, data: borrarProveedorResponse };
     } catch (error) {
       return { success: false, error: error };
     }
